254afbcd9b582e5c9d1b2a84251d32821912115b
piperiver97 <feliperiveros_123@gmail.com>
2024-07-08 14:18:33 +0200

    New project back

---
+++ src/main/java/dev/felipe/support_App/SupportAppApplication.java
@@ -0,0 +1,13 @@
+package dev.felipe.support_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAppApplication.class, args);
+	}
+
+}
